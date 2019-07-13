d271d6efe5b1928e68afdf2dc1c7dacfe951b87d
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2019-07-13 18:44:50 +0500

    updated

--- src/Views/index.blade.php
+++ src/Views/index.blade.php
@@ -11,20 +11,20 @@
 @section('content')
 
     @if (isset($checkResults['counts']))
-        <div class="mx-auto" style="width: 80%; padding:0 12px;">
+        <div class="mx-auto" style="width: 80%; font-size: 16px; padding-bottom:2px;">
             <div class="float-left">
-                <span class="badge-success badge" style="font-size: 12px;">
+                <span class="badge-success badge">
                 Passed: {{$checkResults['counts']['passed_checks_count']}}
                 </span>
-                <span class="badge-danger badge" style="font-size: 12px;">
+                <span class="badge-danger badge">
                 Failed: {{$checkResults['counts']['failed_checks_count']}}
                 </span>
-                <span class="badge-primary badge" style="font-size: 12px;">
+                <span class="badge-primary badge">
                 Total: {{$checkResults['counts']['total_checks_count']}}
                 </span>
             </div>
             <div class="float-right">
-                <span class="badge-success badge" style="font-size: 12px;">Last Checked: {{$lastRun}}</span>
+                <span class="badge-success badge">Last Checked: {{$lastRun}}</span>
             </div>
             <div class="clearfix"></div>
         </div>
@@ -38,19 +38,22 @@
             <thead>
             <tr>
                 <th style="text-align: center;" width="1">#</th>
-                <th>Check Type</th>
                 <th>Check Name</th>
-                <th>Status</th>
-                <th style="text-align: center;" width="1">Run</th>
+                <th style="text-align: center;" width="150">Status</th>
+                <th style="text-align: center;" width="50">Run</th>
             </tr>
             </thead>
 
             <tbody>
             @foreach($checkResults as $type => $checks)
+                <tr>
+                    <td colspan="99" align="center">
+                        <span style="font-size: 12px;" class="badge-primary badge badge-pill">{{strtoupper($type)}}</span>
+                    </td>
+                </tr>
                 @foreach($checks as $index => $check)
                     <tr>
                         <td style="text-align: center; font-weight: bold;">{{++$index}}</td>
-                        <td>{{strtoupper($check['type'])}}</td>
                         <td><strong>{{$check['name']}}</strong></td>
                         @php
                             $isOk = $check['status'] == 1;
@@ -58,7 +61,7 @@
                             $icon = $isOk ? 'success' : 'danger';
                             $popover = $isOk ? '' : 'tabindex="0" data-toggle="popover" data-trigger="focus" title="Error Details" data-content="' . $check['error'] . '"';
 
-                            echo "<td><span style='font-size: 14px;' ' . $popover . ' class='col-sm-12 badge badge-$icon'>$text</span></td>";
+                            echo "<td style='text-align: center;'><span style='font-size: 14px; padding-bottom:6px;' ' . $popover . ' class='col-sm-10 badge badge-$icon'>$text</span></td>";
                         @endphp
                         <td style="text-align: center;">
                         <span
@@ -68,7 +71,7 @@
                                 data-placement="top"
                                 title="Run this check"
                                 style="font-size: 10px;">
-                            <i class="fa fa-play" style="font-size: 10px;"></i>
+                            <i class="fa fa-play" style="font-size: 14px;"></i>
                         </span>
                         </td>
                     </tr>
@@ -78,104 +81,7 @@
         </table>
     </div>
 
-    <div id="overlay">
-        <div class="spinner"></div>
-        <span class="overlay-message">Working, please wait...</span>
-    </div>
-
 @endsection
-
-@push('styles')
-    <style>
-        #overlay {
-            position: fixed;
-            display: none;
-            width: 100%;
-            height: 100%;
-            top: 0;
-            left: 0;
-            right: 0;
-            bottom: 0;
-            background-color: rgba(0, 0, 0, 0.7);
-            z-index: 9999999999;
-        }
-
-        #overlay .overlay-message {
-            position: fixed;
-            left: 50%;
-            top: 57%;
-            height: 100px;
-            width: 250px;
-            margin-left: -120px;
-            margin-top: -50px;
-            color: #fff;
-            font-size: 20px;
-            text-align: center;
-            font-weight: bold;
-        }
-
-        .spinner {
-            position: fixed;
-            left: 50%;
-            top: 40%;
-            height: 80px;
-            width: 80px;
-            margin-left: -40px;
-            margin-top: -40px;
-            -webkit-animation: rotation .9s infinite linear;
-            -moz-animation: rotation .9s infinite linear;
-            -o-animation: rotation .9s infinite linear;
-            animation: rotation .9s infinite linear;
-            border: 6px solid rgba(255, 255, 255, .15);
-            border-top-color: rgba(255, 255, 255, .8);
-            border-radius: 100%;
-        }
-
-        @-webkit-keyframes rotation {
-            from {
-                -webkit-transform: rotate(0deg);
-            }
-            to {
-                -webkit-transform: rotate(359deg);
-            }
-        }
-
-        @-moz-keyframes rotation {
-            from {
-                -moz-transform: rotate(0deg);
-            }
-            to {
-                -moz-transform: rotate(359deg);
-            }
-        }
-
-        @-o-keyframes rotation {
-            from {
-                -o-transform: rotate(0deg);
-            }
-            to {
-                -o-transform: rotate(359deg);
-            }
-        }
-
-        @keyframes rotation {
-            from {
-                transform: rotate(0deg);
-            }
-            to {
-                transform: rotate(359deg);
-            }
-        }
-
-        table.dataTable tr.group td {
-            background-image: radial-gradient(#fff, #eee);
-            border: none;
-            text-align: center;
-            font-weight: bold;
-            font-size: 16px;
-        }
-    </style>
-@endpush
 
 @push('scripts')
     <script>
@@ -188,33 +94,12 @@
 
         $('[data-toggle="tooltip"]').tooltip();
 
-        $('.table').DataTable({
-            "order": [],
-            "responsive": true,
-            "paging": false,
-            "info": false,
-            "searching": false,
-            "sort": false,
-            "pageLength": 100,
-            "autoWidth": true,
-            aoColumnDefs: [
-                {
-                    bSortable: false,
-                    aTargets: [-1]
-                },
-                {
-                    visible: false,
-                    aTargets: [1]
-                }
-            ],
-            rowGroup: {
-                dataSrc: 1
-            }
-        });
-
         // refresh all checks
         $('#btnRefresh').click(function () {
-            $('#overlay').show();
+            $('body').loading({
+                message: 'Running, please wait...',
+                stoppable: false
+            });
 
             $.get('{{route('servermonitor_refresh_all')}}', function () {
                 window.location.reload();
@@ -223,10 +108,13 @@
 
         // refresh single check
         $('.refresh').click(function () {
-            $('#overlay').show();
+            $('body').loading({
+                message: 'Running, please wait...',
+                stoppable: false
+            });
 
             $.get('{{route('servermonitor_refresh')}}', {check: $(this).data('checker')}, function (result) {
-                $('#overlay').hide();
+                $('body').loading('stop');
 
                 if (result.status) {
                     swal("Passed", "Check Passed Successfully!", "success");
--- src/Views/layout/layout.blade.php
+++ src/Views/layout/layout.blade.php
@@ -7,47 +7,30 @@
 
     <title>ServerMonitor</title>
 
-    <!-- Bootstrap core CSS -->
     <link rel="stylesheet"
           href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css"
           integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4"
           crossorigin="anonymous">
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap4.min.css">
-    <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.1/css/responsive.bootstrap4.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/jquery-easy-loading@1.3.0/dist/jquery.loading.css">
 
     <style>
-        body {
-            font-family: sans-serif;
-            font-size: 0.8rem;
-            line-height: 1.0;
-            background-color: #cccccc;
-            background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8' viewBox='0 0 8 8'%3E%3Cg fill='%23dddddd' fill-opacity='0.4'%3E%3Cpath fill-rule='evenodd' d='M0 0h4v4H0V0zm4 4h4v4H4V4z'/%3E%3C/g%3E%3C/svg%3E");
+        body, .card-body {
+            background: #cccccc url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8' viewBox='0 0 8 8'%3E%3Cg fill='%23dddddd' fill-opacity='0.4'%3E%3Cpath fill-rule='evenodd' d='M0 0h4v4H0V0zm4 4h4v4H4V4z'/%3E%3C/g%3E%3C/svg%3E");
         }
 
         .card-body {
-            background-color: #cccccc;
-            background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8' viewBox='0 0 8 8'%3E%3Cg fill='%23dddddd' fill-opacity='0.4'%3E%3Cpath fill-rule='evenodd' d='M0 0h4v4H0V0zm4 4h4v4H4V4z'/%3E%3C/g%3E%3C/svg%3E");
             padding: 0.50rem;
         }
 
-        .card-header {
-            padding: 10px !important;
-        }
-
-        .table td, .table th {
-            padding: .50rem;
-            vertical-align: middle;
-        }
-
         .table td {
+            vertical-align: middle;
             background: #fff;
         }
 
         .table thead {
-            background-image: linear-gradient(#eee, #ddd);
+            background: #eee;
         }
 
         .card {
@@ -65,8 +48,6 @@
             background-size: 100% 20px;
         }
     </style>
-
-    @stack('styles')
 </head>
 
 <body>
@@ -92,30 +73,24 @@
     </div>
 </main>
 
-<!-- Bootstrap core JavaScript
-================================================== -->
 <script
-    src="https://code.jquery.com/jquery-3.4.1.min.js"
-    integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
-    crossorigin="anonymous">
+        src="https://code.jquery.com/jquery-3.4.1.min.js"
+        integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+        crossorigin="anonymous">
 </script>
 <script
-    src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"
-    integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ"
-    crossorigin="anonymous">
+        src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"
+        integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ"
+        crossorigin="anonymous">
 </script>
 <script
-    src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"
-    integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm"
-    crossorigin="anonymous">
+        src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"
+        integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm"
+        crossorigin="anonymous">
 </script>
 
-<script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/1.10.16/js/dataTables.bootstrap4.min.js"></script>
-<script src="https://cdn.datatables.net/responsive/2.2.1/js/dataTables.responsive.min.js"></script>
-<script src="https://cdn.datatables.net/responsive/2.2.1/js/responsive.bootstrap4.min.js"></script>
-<script src="https://cdn.datatables.net/rowgroup/1.0.4/js/dataTables.rowGroup.min.js"></script>
 <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/jquery-easy-loading@1.3.0/dist/jquery.loading.min.js"></script>
 
 @stack('scripts')
 
